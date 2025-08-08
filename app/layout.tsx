e916b9b0ccfb077251d73bfe66a775cf1bd7fb71
alphacrash <sudhansu.ace@gmail.com>
2025-08-08 13:04:43 +0530

    Added Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,12 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
